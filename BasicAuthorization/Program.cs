05a0ad537bbb2c14bf125a0c9d8e27f19e1f2bca
Johnny Oshika <johnnyoshika@gmail.com>
2017-07-11 23:36:54 -0700

    Add parser.

--- BasicAuthorization/Program.cs
+++ BasicAuthorization/Program.cs
@@ -18,7 +18,21 @@ namespace BasicAuthorization
 
             var builder = new BasicAuthenticationBuilder(username, password);
             Console.WriteLine(builder.GetHeader());
-            Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Value to Parse:");
+            string value = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                var parser = new BasicAuthenticationParser(value);
+                if (parser.TryParse(out string result))
+                    Console.WriteLine($"Parsed result wrapped in singlel quotes: '{result}'");
+                else
+                    Console.WriteLine("Parsing error!");
+            }
+
+            Console.WriteLine("End!");
         }
     }
 
@@ -38,6 +52,41 @@ namespace BasicAuthorization
             var encoding = new ASCIIEncoding();
             byte[] auth = encoding.GetBytes(String.Format("{0}:{1}", Username, Password));
             return "Authorization: Basic " + Convert.ToBase64String(auth);
+        }
+    }
+
+    public class BasicAuthenticationParser
+    {
+        public BasicAuthenticationParser(string value)
+        {
+            Value = value;
+        }
+
+        string Value { get; }
+
+        string NormalizedValue =>
+            Value.StartsWith("Basic ")
+            ? Value.Substring("Basic ".Length).Trim()
+            : Value;
+
+        public string Parse() =>
+            TryParse(out string result)
+                ? result
+                : throw new ArgumentException();
+
+        public bool TryParse(out string result)
+        {
+            result = null;
+            try
+            {
+                var encoding = new ASCIIEncoding();
+                result = encoding.GetString(Convert.FromBase64String(NormalizedValue));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
